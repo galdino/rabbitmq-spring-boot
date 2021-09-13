cc0911f765007c11b5507d7c31500518baaadb8a
Fernando Galdino <fernando.galdino@meta.com.br>
2021-09-13 14:50:24 -0300

    Add json converters

--- src/main/java/br/com/rabbitmqexample/PracticalTipMessage.java
+++ src/main/java/br/com/rabbitmqexample/PracticalTipMessage.java
@@ -1,14 +1,16 @@
 package br.com.rabbitmqexample;
 
-import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class PracticalTipMessage implements Serializable {
+public class PracticalTipMessage {
 	
 	private final String text;
 	private final int priority;
 	private final boolean secret;
 	
-	public PracticalTipMessage(final String text, int priority, boolean secret) {
+	public PracticalTipMessage(@JsonProperty("text") final String text, 
+							   @JsonProperty("priority") final int priority,
+							   @JsonProperty("secret") final boolean secret) {
 		this.text = text;
 		this.priority = priority;
 		this.secret = secret;
